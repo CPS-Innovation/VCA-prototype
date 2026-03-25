573b0c29117a563445603d1d4232f90e7c656d7f
Sam Wooldridge <s.wooldridge@kainos.com>
2026-03-25 14:07:44 +0000

    ad hoc task and service lead

---
+++ app/assets/javascripts/delivery/wat3/update-manual-task.js
@@ -0,0 +1,26 @@
+// Errors
+function validateForm() {
+    $('#error-form-group-1').removeClass('govuk-form-group--error');
+    $('#error-message-1').remove();
+    $('#error-summary').remove();
+
+    var taskAction = $('[name=taskAction]');
+
+    if (!taskAction.is(':checked')) {
+        // Error form group styling
+        $('#error-form-group-1').addClass('govuk-form-group--error');
+
+        // Error message
+        $('#radio-group').before('<p id="error-message-1" class="govuk-error-message"><span class="govuk-visually-hidden">Error:</span> Select if you want to mark the task as complete</p>');
+
+        // Error summary
+        $('.govuk-heading-l').first().before(
+            '<div id="error-summary" class="govuk-error-summary" aria-labelledby="error-summary-title" role="alert" tabindex="-1" data-module="govuk-error-summary"><h2 class="govuk-error-summary__title" id="error-summary-title">There is a problem</h2><div class="govuk-error-summary__body"><ul class="govuk-list govuk-error-summary__list"><li><a href="#error-message-1">Select if you want to mark the task as complete</a></li></ul></div></div>'
+        );
+        $('#error-summary').focus();
+
+        return false;
+    }
+
+    return true;
+}
